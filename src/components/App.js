1318b31ccd5c25704d55b1db474ea1a2c9e102f3
Jing Cao <jingcao.me@gmail.com>
2019-05-08 19:26:43 -0400

    added functions to add rows and pick colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
-
-import store from '../store';
+import React, { Component } from "react";
+import store, { addRow, changeColor } from "../store";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    this.handleRow = this.handleRow.bind(this);
   }
+
   componentDidMount() {
     this.unsubscribe = store.subscribe(() => this.setState(store.getState()));
   }
+
   componentWillUnmount() {
     this.unsubscribe();
   }
+
+  handleRow() {
+    store.dispatch(addRow());
+  }
+
+  handleColorChange(event) {
+    store.dispatch(changeColor(event.target.value));
+  }
+
   render() {
     return (
       <div id="pixelate">
         <h1>Pixelate</h1>
         <div>
-          <button type="button" id="add-row">
+          <button type="button" id="add-row" onClick={this.handleRow}>
             Add a row
           </button>
-          <select>
+          <select onChange={this.handleColorChange}>
             <option value="red">Red</option>
             <option value="orange">Orange</option>
             <option value="yellow">Yellow</option>
@@ -34,7 +45,17 @@ export default class App extends Component {
             <option value="brown">Brown</option>
           </select>
         </div>
-        <table />
+        <table>
+          <tbody>
+            {this.state.grid.map((row, i) => (
+              <tr key={i}>
+                {row.map((cell, j) => (
+                  <td key={j} className={cell} />
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
--- src/store.js
+++ src/store.js
@@ -4,24 +4,46 @@
 // Much like requests in express pass from middleware to middleware, actions in redux
 // pass from middleware to middleware. The loggerMiddleware gets a chance to see
 // actions before they are processed. They get in the middle, hence, middleware.
-import { createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import loggerMiddleware from "redux-logger";
 
 // We'll soon revisit the initial state of this application.
-const initialState = {};
+const initialState = {
+	grid: [Array(20).fill("")],
+	color: "red"
+};
 
 // ACTION TYPES
 /* we'll add some action types soon */
+const ADDROW = "ADDROW";
+const SELECTCOLOR = "SELECTCOLOR";
 
 // ACTION CREATORS
 /* we'll also add the corresponding action creators */
+export const addRow = () => ({ type: ADDROW });
+export const changeColor = color => ({ type: SELECTCOLOR, color: color });
 
 // And we'll revisit this reducer.
 function reducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+	const newState = Object.assign({}, state);
+	switch (action.type) {
+		case ADDROW:
+			//this does not work!
+			//return { grid: grid.push(Array(20).fill("")) };
+			//add new state
+			const newGrid = newState.grid.concat([Array(20).fill("")]);
+			newState.grid = newGrid;
+			break;
+		//return { ...state, grid: [...state.grid, Array(20).fill("")] };
+		case SELECTCOLOR:
+			//change store.color to new color
+			const newColor = action.color;
+			newState.color = newColor;
+			break;
+		default:
+			return newState;
+	}
+	return newState;
 }
 
 const store = createStore(reducer, applyMiddleware(loggerMiddleware));
